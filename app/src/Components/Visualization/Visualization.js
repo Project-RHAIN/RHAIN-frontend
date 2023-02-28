653bc75419bc5cbc6ee7708f9e9887751053c15f
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-02-28 01:01:30 -0800

    Added sample graph

--- app/src/Components/Visualization/Visualization.js
+++ app/src/Components/Visualization/Visualization.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Heading from "../Common/Heading/Heading";
 import BasicTabs from "../Common/BasicTabs/BasicTabs";
+import { HospitalAdmissions } from "../Graphs/Graphs";
 
 const Visualization = () => {
+
+    const tempTabs = [
+        {
+            label: 'Hospitalization Rate',
+            component: <HospitalAdmissions />
+            // component: <>Hospital Admissions</>
+        },
+        {
+            label: 'Affordability',
+            component: <>Affordability</>
+        },
+        {
+            label: 'Pollution Levels',
+            component: <>Pollution Levels</>
+        },
+        {
+            label: 'Livability Factors',
+            component: <>Livability Factors</>
+        },
+    ]
 
     return (
         <React.Fragment>
             <Heading>Visualizations</Heading>
-            <BasicTabs />
+            <BasicTabs tabData={tempTabs} />
         </React.Fragment>
     )
 }
