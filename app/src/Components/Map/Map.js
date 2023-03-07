28e509b06bc86e4b8c7b136dd612b221c173cc6c
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-03-06 21:12:57 -0800

    Minor change for cali

--- app/src/Components/Map/Map.js
+++ app/src/Components/Map/Map.js
@@ -3,10 +3,14 @@ import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simp
 import usCounties from 'us-atlas/counties-10m.json';
 import usStates from 'us-atlas/states-10m.json';
 import stateCords from '../../Data/State_Coordinates.json'
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import './Map.scss'
 
 const Map = () => {
       
-    const curState = 'California';       
+    const curState = 'California';  
+         
     const selectedState = usStates.objects.states.geometries.find(state => state.properties.name === curState);
     const selectedStateCord = stateCords.find(obj => obj.state === curState)
     // console.log(selectedStateCord)    
@@ -32,12 +36,14 @@ const Map = () => {
     }
 
     return (
-    <div>
+    <div className="mainMap">        
+        <AddCircleIcon className="button" onClick={handleZoomIn}/>                
+        <RemoveCircleIcon className="button bottom-button" onClick={handleZoomOut}/>        
         <ComposableMap 
         projection="geoAlbersUsa"
         // width='800'
         // height='600'
-        // style={{ width: "100%", height: "auto" }} 
+        style={{ width: "100%", height: "100%" }} 
         >
             <ZoomableGroup
                 zoom={zoom} 
@@ -50,6 +56,11 @@ const Map = () => {
                     return (                                        
                     <Geography 
                         onClick={() => clickState(geo)}
+                        style={{
+                            default: { outline: "none" },
+                            hover: { outline: "none", fill: '#d4defa' },
+                            pressed: { outline: "none"},
+                          }}
                         key={geo.rsmKey} 
                         geography={geo} 
                         fill="#DDD" 
@@ -76,12 +87,15 @@ const Map = () => {
                         style={{
                             default: {
                                 fill: "#a3c7ff",
+                                outline: 'none'
                             },                        
                             hover: {
                             fill: "#F53",
+                            outline: 'none'
                             },
                             pressed: {
                             fill: "#015ff",
+                            outline: 'none'
                             },
                         }}                        
                     />
