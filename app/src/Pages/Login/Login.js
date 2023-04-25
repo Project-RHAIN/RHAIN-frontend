dd22eef814602c8b7d2712055c1de78e86965520
Sangeeta Rao <sangeeta@Sangeetas-MacBook-Pro.local>
2023-04-25 13:25:05 -0700

    Added GoogleAuth Login to Login Page

--- app/src/Pages/Login/Login.js
+++ app/src/Pages/Login/Login.js
@@ -3,9 +3,17 @@ import './Login.scss'
 import { Grid, Button } from "@mui/material";
 import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 import TextField from '@mui/material/TextField';
+import { GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
 
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+    let path = `/home`; 
+    navigate(path);
+    }
     // const [password, setPassword]
 
     return (
@@ -14,7 +22,15 @@ const Login = () => {
 
             <Grid item sm={8} className='column1'>  
                 <div className='left-container'>
-                <h1>Regional Healthcare Analysis and Improvement Network</h1>
+                {/* <GoogleLogin onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />; */}
+
+                <h1>Regional Healthcare Analysis and Improvement Network </h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. <br /> Maecenas accumsan lacus vel facilisis volutpat est velit. Nec ultrices dui sapien eget mi proin sed libero enim. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Sit amet massa vitae tortor. Commodo ullamcorper a lacus vestibulum sed arcu. Massa tincidunt dui ut ornare lectus sit amet.</p>
                 </div>          
             </Grid>
@@ -38,8 +54,16 @@ const Login = () => {
                         required
                         fullWidth
                     />   
-                    <Button variant="contained" disableElevation className='login-button'>
-                        Login
+                    <Button variant="contained" disableElevation className='login-button' onClick={routeChange} >
+                    <GoogleOAuthProvider clientId="888396688109-n3ms9snv8n9jbpn7bam27kvt4mce87gp.apps.googleusercontent.com">
+                    <GoogleLogin onSuccess={credentialResponse => {
+                        console.log(credentialResponse );
+                        navigate('/home');}}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                    />
+                    </GoogleOAuthProvider>
                     </Button>
                 </div>        
             </Grid>
