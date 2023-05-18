8b30b499985c0f6c538f9a8798ebb9a2b310cf0d
Rithesh <keatz68@gmail.com>
2023-05-18 11:33:52 -0700

    Updated dependencies

--- app/src/Pages/Login/Login.js
+++ app/src/Pages/Login/Login.js
@@ -30,7 +30,15 @@ const Login = () => {
                     }}
                 />; */}                                
                 <h1>Regional Healthcare Analysis and Improvement Network </h1>
-                <p>Our goal is to leverage the power of big data methodologies to provide insights into the issues surrounding healthcare delivery on a region-by-region basis. We aim to identify the primary problems of quality, accessibility, and affordability, and assign a score for each aspect for each region. By aggregating multiple sources of data, such as doctor/hospital reviews, income data, and proximity to hospitals, we analyze the raw data to produce valuable metrics that define the healthcare standards in a region. <br />Our user-friendly dashboard provides easy access to these insights, helping policymakers and healthcare professionals recognize the necessary actions to improve the system. Join us on our mission to make healthcare accessible and affordable for everyone.</p>
+                <p>Our goal is to leverage the power of big data methodologies to provide insights into the issues 
+                surrounding healthcare delivery on a region-by-region basis. 
+                We aim to identify the primary problems of quality, accessibility, and affordability, and assign a score for each aspect 
+                for each region. 
+                By aggregating multiple sources of data, such as doctor/hospital 
+                reviews, income data, and proximity to hospitals, 
+                we analyze the raw data to produce valuable metrics that define the healthcare standards in a region. 
+                <br />Our user-friendly dashboard provides easy access to these insights, helping policymakers and healthcare professionals recognize the necessary actions to improve the system. 
+                Join us on our mission to make healthcare accessible and affordable for everyone.</p>
                 </div>          
             </Grid>
 
@@ -38,7 +46,7 @@ const Login = () => {
                 <div className='login-section'>
                     {/* <HealthAndSafetyIcon className='logo' /> */}                    
                     <img src={logo} alt="Logo" height="70px"/>
-                    <h1>Login</h1>
+                    <h1>Sign in here!</h1>
                     <TextField
                         label="Email"
                         type="email"
