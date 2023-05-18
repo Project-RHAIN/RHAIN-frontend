08db5acda5470fab84901b4ec493551766bed7c6
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-05-18 12:47:20 -0700

    Commented out register from app needs fix

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard/Dashboard';
-// import Login from './Pages/Login/Login';
-import Register from './Pages/Login/Register';
+import Login from './Pages/Login/Login';
+// import Register from './Pages/Login/Register';
 import { ThemeProvider } from '@mui/material';
 import theme from './Theme/customTheme';
 function App() {
@@ -14,7 +14,8 @@ function App() {
       <Router>
         <ThemeProvider theme={theme}>        
         <Routes>
-          <Route path="/" element={<Register />} />
+          {/* <Route path="/" element={<Register />} /> */}
+          <Route path="/" element={<Login />} />
           <Route path="/home" element={<Dashboard />} />          
           <Route path="*" element={<div>Oops no page found</div>} />
         </Routes>
