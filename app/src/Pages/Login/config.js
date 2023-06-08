068d6f9c032af4ceddd647664865bb895825e2d6
Utkarsh Chhapekar <utkarshchhapekar2@gmail.com>
2023-06-08 16:51:08 -0700

    Removed config file for .env

--- app/src/Pages/Login/config.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = 'patINoqscdJqGN99D.da9df089db04370b459a32adefdeb497b54c2c26902a6ae906c4ce71319ad629';
-export const BASE_ID = 'appZKbOG56UrV0Dvp';
-export const TABLE_NAME = 'Users';
